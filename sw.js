b86c6b2333ac9b5cbe3420b42b9a5170d8070cdf
Martin Bokša <martin.boksa@gmail.com>
2019-01-13 18:08:53 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fd2d64c80f173497e5c5.js"
+    "url": "webpack-runtime-ac5af300e893e73b284f.js"
   },
   {
     "url": "app.3ee779054d5c13599323.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "10a03ad5e12ab09fdfb29b596f1aaee4"
+    "revision": "ff4661e5688187991b4a9958a78a6cb4"
   },
   {
     "url": "component---src-pages-404-js-0e371c446872f582d913.js"
